6ea3f3e1c0890a3eef5b3cec3c2d1bf5ced5da9c
ihplus-skmin <skmin@inhandplus.com>
2024-11-08 18:00:30 +0900

    loading2

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,5 @@
+import { resolve } from "path";
+
 export const metadata = {
   title: "Home",
 };
@@ -5,6 +7,7 @@ export const metadata = {
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies () {
+  await new Promise((resolve) => setTimeout(resolve, 5000)) 
   const response = await fetch(URL);
   const json = await response.json();
   return json;
